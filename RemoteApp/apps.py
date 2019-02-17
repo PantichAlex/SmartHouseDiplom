186bbcae9c91931a959f14a38e6d4468495c8535
Alexey <alexeypanta@gmail.com>
2019-02-17 18:47:39 +0800

    n form
    main form

---
+++ RemoteApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RemoteappConfig(AppConfig):
+    name = 'RemoteApp'
--- SmartHouse/settings.py
+++ SmartHouse/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RemoteApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'SmartHouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'RU-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,5 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIRS = (
 
-STATIC_URL = '/static/'
+    os.path.join(BASE_DIR, 'static'),
+
+)
+
+STATIC_URL = "/static/"
