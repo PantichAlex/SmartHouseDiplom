a7f8be037f0e0677d27577333096017bb6160b3c
Alexey <alexeypanta@gmail.com>
2019-02-18 18:26:16 +0800

    сделаны кнопки для устройств

--- RemoteApp/views.py
+++ RemoteApp/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 def index(request):
 
+    HomeParameters=dict()
 
-    return render(request,"RemoteApp/main.html")
+    HomeParameters["temperature"]=22
+    HomeParameters["humidity"]=60
+    HomeParameters["presure"]=761
+    HomeParameters["Controls"]=tuple(range(30))
+
+    return render(request,"RemoteApp/main.html", context=HomeParameters)
 # Create your views here.
 
