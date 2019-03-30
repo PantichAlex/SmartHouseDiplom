8a0f0368f657f94f4a999aaf7666e676f5c734fd
Alexey <alexeypanta@gmail.com>
2019-03-30 18:09:26 +0800

    Настроил админку

--- RemoteApp/admin.py
+++ RemoteApp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Rooms,Users,Premissions, UserPemissions, Devices, Macro
 
+
+@admin.register(Rooms)
+class RoomAdmin(admin.ModelAdmin):
+    fields = ("RoomName","Description")
+
+@admin.register(Users)
+class UserAdmin(admin.ModelAdmin):
+    fields=("username","login", "password", "email", "phone")
+
+@admin.register(Premissions)
+class PermissionAdmin(admin.ModelAdmin):
+    fields = ("Description","device")
+
+
+@admin.register(UserPemissions)
+class UserPermissiosAdmin(admin.ModelAdmin):
+    fields = ("user", "permission")
+
+
+@admin.register(Devices)
+class DevicesAdmin(admin.ModelAdmin):
+    fields=("name", "driverPath", "iconPath", "template", "room")
+
+@admin.register(Macro)
+class MacroAdmin(admin.ModelAdmin):
+    fields = ("user","device", "text")
 # Register your models here.
--- RemoteApp/models.py
+++ RemoteApp/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
+class Rooms(models.Model):
+    class Meta:
+        db_table="Rooms"
+        verbose_name="Помещения"
+        verbose_name_plural="Помещения"
+
+    RoomName=models.CharField(verbose_name="Название", max_length=50)
+    Description = models.CharField(verbose_name="Описание", max_length=500)
+
+class Devices(models.Model):
+
+    class Meta:
+        db_table="Devices"
+        verbose_name="Устройства"
+        verbose_name_plural = "Устройства"
+
+    name=models.CharField(verbose_name="Название", max_length=100)
+    driverPath=models.CharField(verbose_name="Путь к драйверу", max_length=300)
+    iconPath=models.CharField(verbose_name="Иконка", max_length=300)
+    template=models.CharField(verbose_name="Шаблон интерфейса", max_length=300)
+    room=models.ForeignKey(Rooms, verbose_name="Помещение", on_delete=models.CASCADE)
 
 class Users(models.Model):
 
     class Meta:
         db_table="Users"
         verbose_name="Пользователи"
+        verbose_name_plural = "Пользователи"
 
     username=models.CharField(verbose_name="Пользователь", unique=True, max_length=50)
     login=models.CharField(verbose_name="Логин", unique=True, max_length=45)
@@ -19,6 +41,7 @@ class Premissions(models.Model):
     class Meta:
         db_table="Permissions"
         verbose_name="Права"
+        verbose_name_plural = "Права"
 
     Description=models.CharField(verbose_name="Описание", max_length=500)
     device=models.ForeignKey(Devices, verbose_name="Устройство", on_delete=models.CASCADE)
@@ -29,37 +52,18 @@ class UserPemissions(models.Model):
     class Meta:
         db_table="UserPermissions"
         verbose_name="Права пользователей"
+        verbose_name_plural = "Права пользователей"
 
     user=models.ForeignKey(Users, on_delete=models.CASCADE)
     permission=models.ForeignKey(Premissions, on_delete=models.CASCADE)
 
 
-
-class Devices(models.Model):
-
-    class Meta:
-        db_table="Devices"
-        verbose_name="Устройства"
-
-    name=models.CharField(verbose_name="Название", max_length=100)
-    driverPath=models.CharField(verbose_name="Путь к драйверу", max_length=300)
-    iconPath=models.CharField(verbose_name="Иконка", max_length=300)
-    template=models.CharField(verbose_name="Шаблон интерфейса", max_length=300)
-    room=models.ForeignKey(Rooms, verbose_name="Помещение")
-
-class Rooms(models.Model):
-    class Meta:
-        db_table="Rooms"
-        verbose_name="Помещения"
-
-    RoomName=models.CharField(verbose_name="Название", max_length=50)
-    Description = models.CharField(verbose_name="Описание", max_length=500)
-
 class Macro(models.Model):
     class Meta:
         db_table="Macro"
         verbose_name="Пользовательские макросы"
+        verbose_name_plural="Пользовательские макросы"
 
-    user=models.ForeignKey(Users,verbose_name="Пользователь")
-    device=models.ForeignKey(Devices, verbose_name="Устройство")
+    user=models.ForeignKey(Users,on_delete=models.CASCADE,verbose_name="Пользователь")
+    device=models.ForeignKey(Devices, on_delete=models.CASCADE, verbose_name="Устройство")
     text=models.CharField(verbose_name="Текст макроса", max_length=1000)
